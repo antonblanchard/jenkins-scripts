47da83121e30310cf20bce897483a40e8ca489c9
Anton Blanchard <anton@samba.org>
2014-11-28 06:39:35 +1100

    Add kernel build testcase

---
+++ kernel_build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -e
+
+if [ -n "$BIG_ENDIAN" ]; then
+	linux_target=pseries_defconfig
+else
+	linux_target=pseries_le_defconfig
+fi
+
+PARALLEL=-j$(($(nproc) * 2))
+
+ROOT=$(dirname $0)
+
+function finish {
+	rm -rf "$WORKSPACE/linux.build"
+}
+trap finish EXIT
+
+mkdir -p "$WORKSPACE/linux.build"
+cd "$WORKSPACE/linux"
+export KBUILD_OUTPUT="$WORKSPACE/linux.build"
+make $linux_target
+make $PARALLEL vmlinux
+make $PARALLEL zImage
+make $PARALLEL modules
+
+if [ -n "$qemu_testcase" ]; then
+	$ROOT/$qemu_testcase vmlinux
+fi
