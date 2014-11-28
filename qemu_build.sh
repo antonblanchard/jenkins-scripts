1f0e1a67f5007e16af8df5108d29efdcb6220c94
Anton Blanchard <anton@samba.org>
2014-11-28 17:01:10 +1100

    Add qemu build script

---
+++ qemu_build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+
+OPTS="--target-list=ppc64-softmmu --enable-fdt"
+
+PARALLEL=-j$(($(nproc) * 2))
+
+ROOT=$(dirname $0)
+
+function finish {
+	rm -rf "$WORKSPACE/qemu.build"
+}
+trap finish EXIT
+
+mkdir -p "$WORKSPACE/qemu.build"
+
+cd "$WORKSPACE/qemu.build"
+
+$WORKSPACE/qemu/configure $OPTS
+make $PARALLEL
