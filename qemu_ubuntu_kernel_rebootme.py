acebd1015295f4e1f8e38817c16086fb783a7c27
Anton Blanchard <anton@samba.org>
2014-12-02 08:50:57 +1100

    Add a rebootme test

---
+++ qemu_ubuntu_kernel_rebootme.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import sys
+from qemu_ubuntu_test import qemu_ubuntu_test
+
+
+if len(sys.argv) != 3:
+	print "Usage: qemu_ubuntu_kernel_rebootme.py kernel iterations"
+	sys.exit(1)
+
+kernel=sys.argv[1]
+iterations=int(sys.argv[2])
+
+q = qemu_ubuntu_test(kvm=True, cores=8, threads=8, kernel=kernel, cmdline='root=/dev/sda1 rw', virtio=True)
+q.boot()
+
+for i in range(iterations):
+	q.login()
+	q.child.sendline('shutdown -r now')
+	q.wait_for_login()
