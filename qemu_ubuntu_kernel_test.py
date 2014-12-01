42e8329d747d0415c3532cabc6af113532ce54d2
Anton Blanchard <anton@samba.org>
2014-12-02 08:45:18 +1100

    Fix simple_test()

--- qemu_ubuntu_kernel_test.py
+++ qemu_ubuntu_kernel_test.py
@@ -12,20 +12,20 @@ kernel=sys.argv[1]
 
 print "Testing PAPR virtual IO"
 q = qemu_ubuntu_test(kvm=True, kernel=kernel, cmdline='root=/dev/sda1 rw')
-q.simple_test(q, timeout=30)
+q.simple_test(timeout=30)
 q.close()
 
 print "Testing virtio virtual IO"
 q = qemu_ubuntu_test(kvm=True, kernel=kernel, cmdline='root=/dev/vda1 rw', virtio=True)
-q.simple_test(q, timeout=30)
+q.simple_test(timeout=30)
 q.close()
 
 print "Testing SMP"
 q = qemu_ubuntu_test(kvm=True, cores=8, threads=8, kernel=kernel, cmdline='root=/dev/sda1 rw')
-q.simple_test(q, timeout=30)
+q.simple_test(timeout=30)
 q.close()
 
 print "Testing QEMU full emulation"
 q = qemu_ubuntu_test(kvm=False, kernel=kernel, cmdline='root=/dev/sda1 rw')
-q.simple_test(q)
+q.simple_test()
 q.close()
