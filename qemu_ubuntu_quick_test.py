c7f3df11c87f52532be590d527812c91cacbe337
Anton Blanchard <anton@samba.org>
2015-01-04 14:52:37 +1100

    Fix qemu_ubuntu_quick_test.py

--- qemu_ubuntu_quick_test.py
+++ qemu_ubuntu_quick_test.py
@@ -5,12 +5,12 @@ from qemu_ubuntu_test import qemu_ubuntu_test
 
 
 if len(sys.argv) != 2:
-	print "Usage: qemu_ubuntu_quick_test.py qemu_binary"
+	print "Usage: qemu_ubuntu_quick_test.py kernel"
 	sys.exit(1)
 
-qemu=sys.argv[1]
+kernel=sys.argv[1]
 
 print "Testing PAPR virtual IO"
-q = qemu_ubuntu_test(qemu=qemu, kvm=False)
+q = qemu_ubuntu_test(kvm=False, kernel=kernel, cmdline='root=/dev/sda1 rw')
 q.simple_test(timeout=600)
 q.close()
