a40f39d55fb330fbeb6763d0e7ca5204df8467e7
Anton Blanchard <anton@samba.org>
2017-01-05 15:12:30 +1100

    Add virtio to kernel build

--- kernel_build.sh
+++ kernel_build.sh
@@ -18,6 +18,16 @@ cd "$WORKSPACE/linux"
 export KBUILD_OUTPUT="$WORKSPACE/linux.build"
 make $linux_target
 echo CONFIG_ISO9660_FS=y >> "$WORKSPACE/linux.build/.config"
+
+# Build virtio into kernel
+cat << EOF >> "$WORKSPACE/linux.build/.config"
+CONFIG_VIRTIO=y
+CONFIG_VIRTIO_PCI=y
+CONFIG_VIRTIO_BLK=y
+CONFIG_SCSI_VIRTIO=y
+CONFIG_VIRTIO_NET=y
+EOF
+
 make $PARALLEL vmlinux
 make $PARALLEL zImage
 make $PARALLEL modules
