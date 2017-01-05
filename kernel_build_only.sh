e2f4d2b5d62ed6ffd5fd20bf156553d3d4c30b77
Anton Blanchard <anton@samba.org>
2017-01-05 17:11:22 +1100

    Fix perms on kernel_build_only.sh

---
+++ kernel_build_only.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -e
+
+export KBUILD_OUTPUT="$WORKSPACE/linux.build"
+rm -rf $KBUILD_OUTPUT
+
+make pseries_le_defconfig
+echo CONFIG_ISO9660_FS=y >> "$WORKSPACE/linux.build/.config"
+# Build virtio into kernel  
+cat << EOF >> "$WORKSPACE/linux.build/.config"
+CONFIG_VIRTIO=y
+CONFIG_VIRTIO_PCI=y 
+CONFIG_VIRTIO_BLK=y 
+CONFIG_SCSI_VIRTIO=y
+CONFIG_VIRTIO_NET=y 
+EOF
+
+make -j32
+
+make INSTALL_MOD_PATH="$WORKSPACE/install" modules_install
+( cd "$WORKSPACE/install" ; find . | cpio -oVH newc | gzip -9 > ../modules.cpio.gz )
+
+cd tools/perf
+make
+
+# Reduce on disk space
+cp -f $WORKSPACE/linux.build/vmlinux $WORKSPACE/vmlinux
+rm -rf "$KBUILD_OUTPUT" "$WORKSPACE/install"
