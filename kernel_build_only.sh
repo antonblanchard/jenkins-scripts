fa4400a8d9c3cb1c3c0d80bc2475838773584d49
Anton Blanchard <anton@samba.org>
2017-01-06 17:08:22 +1100

    Fix perf build dir issue
    perf wants O= on the make command line

--- kernel_build_only.sh
+++ kernel_build_only.sh
@@ -1,20 +1,20 @@
 #!/bin/bash -e
 
 export KBUILD_OUTPUT="$WORKSPACE/linux.build"
-export OUTPUT="$WORKSPACE/perf.build"
-rm -rf "$KBUILD_OUTPUT" "$WORKSPACE/install" "$OUTPUT"
+export PERF_OUTPUT="$WORKSPACE/perf.build"
+rm -rf "$KBUILD_OUTPUT" "$PERF_OUTPUT" "$WORKSPACE/install"
 
 cd "$WORKSPACE/linux"
 
 make pseries_le_defconfig
 echo CONFIG_ISO9660_FS=y >> "$WORKSPACE/linux.build/.config"
-# Build virtio into kernel  
+# Build virtio into kernel
 cat << EOF >> "$WORKSPACE/linux.build/.config"
 CONFIG_VIRTIO=y
-CONFIG_VIRTIO_PCI=y 
-CONFIG_VIRTIO_BLK=y 
+CONFIG_VIRTIO_PCI=y
+CONFIG_VIRTIO_BLK=y
 CONFIG_SCSI_VIRTIO=y
-CONFIG_VIRTIO_NET=y 
+CONFIG_VIRTIO_NET=y
 EOF
 
 make -j$(nproc)
@@ -22,11 +22,11 @@ make -j$(nproc)
 make INSTALL_MOD_PATH="$WORKSPACE/install" modules_install
 ( cd "$WORKSPACE/install" ; find . | cpio -oVH newc | gzip -9 > ../modules.cpio.gz )
 
-mkdir "$OUTPUT"
+mkdir "$PERF_OUTPUT"
 cd tools/perf
-make
+make O="$PERF_OUTPUT"
 
 # Reduce on disk space
 cp -f "$KBUILD_OUTPUT/vmlinux" $WORKSPACE/vmlinux
-cp -f "$OUTPUT/perf" "$WORKSPACE/perf"
-rm -rf "$KBUILD_OUTPUT" "$WORKSPACE/install" "$OUTPUT"
+cp -f "$PERF_OUTPUT/perf" "$WORKSPACE/perf"
+rm -rf "$KBUILD_OUTPUT" "$PERF_OUTPUT" "$WORKSPACE/install"
